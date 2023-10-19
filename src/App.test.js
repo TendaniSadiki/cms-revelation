68bfa4fb50aad8ae38440cf4baa6e9bf2e1dccc9
TendaniSadiki <tendanisadiki16@gmail.com>
2023-10-19 18:53:43 +0200

    error

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { act } from 'react-dom/test-utils';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  act(() => {  // Wrap state updates inside act
-    expect(linkElement).toBeInTheDocument();
-  });
+  expect(linkElement).toBeInTheDocument();
 });
