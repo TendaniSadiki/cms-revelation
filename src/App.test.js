b62c78cd8b626960cea7ddbdf36d9fef0c356095
TendaniSadiki <tendanisadiki16@gmail.com>
2023-10-19 17:08:41 +0200

    app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // Use await to wait for the component to render and the text to become available
+  const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
